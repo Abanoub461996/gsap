fa354fe8abb45da67fc5251577bc491e76b49dfc
Abanoub461996 <pophtc.aa@gmail.com>
2024-07-09 23:18:41 +0300

    first commit

---
+++ src/pages/LandingPage/LandingPage.style.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const LandingPageWrapper = styled.div`
+  width: 100%;
+    div{
+        width: 100%;
+        height: 100vh;
+    }
+  .image1{background-color:red}
+  .image2{background-color:black}
+  .image3{background-color:cyan}
+
+`;
